bdf1d3e2ed6457ffa41d25e4534fd6e9f74d7b2d
Hassan <hassanalihazaraa@gmail.com>
2020-08-04 13:07:38 +0200

    created customer model

---
+++ Model/Customer.model.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+class Customer
+{
+    private int $id;
+    private string $name;
+    private int $groupId;
+
+    public function __construct(int $id, string $name, int $groupId)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->groupId = $groupId;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getGroupId(): int
+    {
+        return $this->groupId;
+    }
+}
