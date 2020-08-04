e9bf21d3224d8f6a6a7cf13b11be6e38593ab11e
Hassan <hassanalihazaraa@gmail.com>
2020-08-04 14:30:01 +0200

    changed database connection location to model

---
+++ Model/DatabaseConnection.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+//database connection
+function openConnection(): PDO
+{
+    $driverOptions = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
+    require_once '../hiddenResources/credentials.php';
+
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
+}
+
+$pdo = openConnection();
--- index.php
+++ index.php
@@ -6,20 +6,5 @@ require_once 'Model/Customer.model.php';
 require_once 'Model/CustomerGroup.model.php';
 require_once 'Model/Product.model.php';
 
-//database connection
-function openConnection(): PDO
-{
-    $driverOptions = [
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    ];
-
-    require_once 'hiddenResources/credentials.php';
-
-    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
-}
-$pdo = openConnection();
-
 $controller = new CalculatorController();
 $controller->render();
