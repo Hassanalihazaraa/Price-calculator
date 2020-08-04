52e02758d562fb205a5f283298dc5d08da95efc2
Yoann Stas <yoann.stas@me.com>
2020-08-04 17:11:33 +0200

    footer and header

--- View/Calculator.view.php
+++ View/Calculator.view.php
@@ -1,21 +1,29 @@
 <?php
 declare(strict_types=1);
 ?>
-<!doctype html>
-<html lang="en">
+
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Price calculator</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
+    <link rel="stylesheet" href="includes/Style.css">
 </head>
 <body>
-<section>
-    <h1 style="text-align: center">Price calculator</h1>
-    <form method="post" action="">
-        <label></label>
-    </form>
+<?php
+?>
+?>
+<section class="section">
+    <div class="container">
+        <h1 class="title">
+            Hello World
+        </h1>
+        <p class="subtitle">
+            My first website with <strong>Bulma</strong>!
+        </p>
+    </div>
 </section>
 </body>
 </html>
---
+++ View/Homepage.php
@@ -0,0 +1,7 @@
+<?php require 'includes/Header.php' ?>
+
+    <section>
+        <p>here comes the content</p>
+    </section>
+
+<?php require 'includes/Footer.php' ?>
