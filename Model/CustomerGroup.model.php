142768de35d46dca424a5aedf9725ba699c73087
Hassan <hassanalihazaraa@gmail.com>
2020-08-04 13:08:07 +0200

    created customer_group model

---
+++ Model/CustomerGroup.model.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+class CustomerGroup
+{
+    private int $id;
+    private string $name;
+    private int $fixed_discount;
+    private int $variable_discount;
+    private int $group_id;
+
+    public function __construct(int $id, string $name, int $fixed_discount, int $variable_discount, int $group_id)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->fixed_discount = $fixed_discount;
+        $this->variable_discount = $variable_discount;
+        $this->group_id = $group_id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getFixedDiscount(): int
+    {
+        return $this->fixed_discount;
+    }
+
+    public function getVariableDiscount(): int
+    {
+        return $this->variable_discount;
+    }
+
+    public function getGroupId(): int
+    {
+        return $this->group_id;
+    }
+}
