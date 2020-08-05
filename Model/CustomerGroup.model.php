121d37055332af9aeedcd8c66e26dad97aefb69b
Hassan <hassanalihazaraa@gmail.com>
2020-08-05 10:59:47 +0200

    make properties shorter

--- Model/CustomerGroup.model.php
+++ Model/CustomerGroup.model.php
@@ -3,24 +3,26 @@ declare(strict_types=1);
 
 class CustomerGroupModel
 {
-    private int $id;
+    private int $id, $parent_id, $fixed_discount, $variable_discount;
     private string $name;
-    private int $fixed_discount;
-    private int $variable_discount;
-    private int $group_id;
 
-    public function __construct(int $id, string $name, int $fixed_discount, int $variable_discount, int $group_id)
+    public function __construct(int $id, string $name, int $fixed_discount, int $variable_discount, int $parent_id)
     {
         $this->id = $id;
         $this->name = $name;
         $this->fixed_discount = $fixed_discount;
         $this->variable_discount = $variable_discount;
-        $this->group_id = $group_id;
+        $this->parent_id = $parent_id;
     }
 
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function getParentId(): int
+    {
+        return $this->parent_id;
     }
 
     public function getName(): string
@@ -36,10 +38,5 @@ class CustomerGroupModel
     public function getVariableDiscount(): int
     {
         return $this->variable_discount;
-    }
-
-    public function getGroupId(): int
-    {
-        return $this->group_id;
     }
 }
