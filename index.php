b323cf563825a0e1215d88c3b77e50bf4164264d
Yoann Stas <yoann.stas@me.com>
2020-08-06 09:53:57 +0200

    css fixed

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-
+/*
 require_once 'Controller/Calculator.controller.php';
 require_once 'Model/Customer.model.php';
 require_once 'Model/CustomerGroup.model.php';
@@ -8,3 +8,5 @@ require_once 'Model/Product.model.php';
 
 $controller = new CalculatorController();
 $controller->render();
+*/
+require 'View/Homepage.php';
